7083b0c3604d159a91691fb18609623b18b50886
Alan <67904137+AlanER404@users.noreply.github.com>
2021-04-03 20:52:20 +0200

    comments in index.js

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
-let screen = document.getElementById('screen'), canvas, c, rScreen;
+let screen = document.getElementById('screen'), canvas, c, rScreen; //starting
 
-const menu = [
+const menu = [ //aray for creating menu, help page, settings, and gameplay page
 `<div>
     <button onclick = "changeS(1)">Game</button>
     <button onclick = "changeS(2)">Help</button>
@@ -12,16 +12,16 @@ const menu = [
 `<p>Luzeru</p>`
 ];
 
-function changeS(which) {
+function changeS(which) { //currect page
     screen.innerHTML = menu[which];
     if (which == 1) setup();
 };
 
-let breakBlock, unBreakBlock, texture;
-var userBullets = [];
-var enemyBullets = [];
+let breakBlock, unBreakBlock, texture; //global variable
+let userBullets = [];
+let enemyBullets = [];
 
-texture = new Image();
+texture = new Image(); //load image
 texture.src = "../texture/texture1.webp"
 
 function setup() { //setup function
@@ -34,23 +34,25 @@ function setup() { //setup function
     canvas.height = rScreen + rScreen/26;
     c = canvas.getContext('2d');
 
+    //reset valutes:
     breakBlock = [];
     unBreakBlock = [];
     userBullets = [];
     enemyBullets = [];
+    bulletID = 1;
 
     mapObj(); //in mapfunc.js
 
     animate();
 };
 
-function animate() {
+function animate() { //main animate function
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawMap();
-    drawBullet();
-    drawTank();
+    drawMap(); //in mapfunc.js
+    drawBullet(); //in bullets.js
+    drawTank(); //in tank.js
 }
 
-changeS(0);
+changeS(0); //set main screen(page)
